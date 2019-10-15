c3bfe80018775e4711c2aaa1a0ff97b15b373755
skadmsfl <noeytt.pk@gmail.com>
2019-10-15 21:53:05 +0700

    Main

--- app/src/main/java/com/example/test4/MainActivity.kt
+++ app/src/main/java/com/example/test4/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.test4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var userInput: EditText? = null
+    private var button: Button? = null
+    private var textView: TextView? = null
+    private var numTimesClicked = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userInput = findViewById<EditText>(R.id.editText2)
+        button = findViewById<Button>(R.id.bottom)
+        textView = findViewById<TextView>(R.id.textView2)
+
+        button?.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                numTimesClicked += 1
+                textView?.append("\nThe button get tapped $numTimesClicked time")
+
+            }
+        })
+
     }
 }
